9a224c151c1e0e768116f59f604954bd8ccff3cd
krishna4412927 <krishna.v@shellkode.com>
2025-10-30 17:50:47 +0530

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = num1 + num2
+product_result = num1 * num2
+average_result = (num1 + num2) / 2
+print(f"Sum: {sum_result}")
+print(f"Product: {product_result}")
+print(f"Average: {average_result}")
